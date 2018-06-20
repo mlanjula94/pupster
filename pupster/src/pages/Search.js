1fc43f798fcd50a6edca8bf152e14e9f341b6fd9
mlanjula94 <mlanjula@gmail.com>
2018-06-20 01:18:06 -0400

    react routes added

--- pupster/src/pages/Search.js
+++ pupster/src/pages/Search.js
@@ -1,13 +1,99 @@
 import React, {Component} from "react";
+import API from "../utils/API";
 
 class Search extends Component {
 
-  state = {}
+  state = {
+    search: "",
+    breedsList: [],
+    breedResults: [],
+    error: ""
+  }
+
+  componentDidMount() {
+    this.getBreedsList();
+  }
+
+  getBreedsList = () => {
+    API.getDogBreeds()
+      .then(res => this.setState({breedsList: res.data.message}))
+      .catch(err => console.log(err));
+  }
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+
+    API.getDogsOfBreed(this.state.search)
+      .then(res => {
+        if (res.data.status === "error") {
+          throw new Error(res.data.message)
+        }
+        this.setState({
+          breedResults: res.data.message,
+          error: ""
+        });
+      })
+      .catch(err => this.setState({error: err.message}))
+  }
 
   render() {
     return (
-      <h1>Search</h1>
+      <div className="container my-5">
+        <h1>Search for a doggo</h1>
+
+        <form>
+          <div className="form-group">
+            <label htmlFor="breedsList">Breed Name:</label>
+            <input 
+              list="breeds" 
+              type="text" 
+              className="form-control"
+              id="breedsList"
+              name="search"
+              value={this.state.search}
+              onChange={this.handleInputChange}
+            />
+            <datalist id="breeds">
+              {this.state.breedsList.map(breed => <option value={breed} key={breed} />)}
+            </datalist>
+
+            <button
+              type="submit"
+              onClick={this.handleFormSubmit}
+              className="btn btn-success"
+            >
+              Search
+            </button>
+          </div>
+        </form>
+
+        <div className="row">
+          {
+            this.state.error 
+            ? 
+            (<h1>{this.state.error}</h1>) 
+            :
+            (
+              this.state.breedResults.map(breedImage => (
+              <div className="col-3" key={breedImage}>
+                <img src={breedImage} alt="Doggo" className="img-fluid" />
+              </div>
+              )
+            )
+            )
+          }
+        </div>
+      </div>
     )
   }
 }
+
 export default Search;
